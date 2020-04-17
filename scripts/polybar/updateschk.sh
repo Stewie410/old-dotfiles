682b25bc339a2824e196963b3cccb4ca8ad48bb9
Stewie410 <stewie410@me.com>
2020-04-17 12:54:15 -0400

    Implemented same logic for both bars' "updates" check

--- scripts/polybar/updateschk.sh
+++ scripts/polybar/updateschk.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # updateschk.sh
 # Author:	Alex Paarfus <stewie410@gmail.com>
-# Date:		2020-04-10
 #
 # Get the total number of available packages since last sync
 # Requires:
@@ -15,6 +14,9 @@ ping -c 1 "8.8.8.8" |& grep --quiet --ignore-case "unreachable" && exit 1
 # Return if required commands not present
 command -v checkupdates >/dev/null || exit 1
 command -v yay >/dev/null || exit 1
+
+# Print if pacman/yay currently running
+( pgrep -x pacman || pgrep -x yay) >/dev/null && { printf '%s\n' "Updating..."; exit; }
 
 # Print Available Updates
 { checkupdates | cut --delimiter=" " --fields=1; yay --query --upgrades --foreign --aur --quiet; } | \
