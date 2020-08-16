6ca9305186deae28d85715a80ac5f4962ac52640
Stewie410 <stewie410@gmail.com>
2020-08-16 17:34:25 -0400

    autostart script formatting

--- scripts/tools/autostart.sh
+++ scripts/tools/autostart.sh
@@ -4,11 +4,16 @@
 #
 # Autostart Applications, regardless of DE/WM
 
+# Applications Autostart
 hsetroot -solid "#000000"
 nitrogen --restore
-picom &
-unclutter --jitter 50 &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-xautolock -time 60 -detectsleep -locker "${HOME}/scripts/tools/lockSession.sh"
-dunst
-#kdeconnect && kdeconntect-indicator
+picom & disown
+unclutter --jitter 50 & disown
+polkit-gnome-authentication-agent-1 & disown
+xautolock -time 60 -detectsleep -locker "${HOME}/scripts/tools/lockSession.sh" & disown
+dunst --config "${HOME}/.config/dunst/dunstrc" & disown
+kdeconnect & disown
+
+# Tray Applications
+#kdeconntect-indicator & disown
+#nm-applet &
