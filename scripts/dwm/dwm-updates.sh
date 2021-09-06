72cf8473db2d5a36930cfe5803b92d80f09321ea
Stewie410 <24593067+Stewie410@users.noreply.github.com>
2021-09-06 11:07:37 -0400

    add a default value to awk invocations + simplify pidof operation

--- scripts/dwm/dwm-updates.sh
+++ scripts/dwm/dwm-updates.sh
@@ -4,32 +4,22 @@
 
 getAUR() {
     aura -Au --dryrun | awk '
-        /->/ && !seen[$1]++ {
-            cnt += 1
-        }
-
-        END {
-            printf "%s", cnt
-        }
+        BEGIN { cnt = 0 }
+        /->/ && !seen[$1]++ { cnt += 1 }
+        END { printf "%s", cnt }
     '
 }
 
 getSTD() {
     aura -Qu | awk '
-        !seen[$1]++ {
-            cnt += 1
-        }
-
-        END {
-            printf "%s", cnt
-        }
+        BEGIN { cnt = 0 }
+        !seen[$1]++ { cnt += 1 }
+        END { printf "%s", cnt }
     '
 }
 
 # Do not get new values if pacman or AUR helpers are running
-for i in "pacman" "yay" "paru" "aura"; do
-    pidof "${i}" &>/dev/null && exit
-done
+pidof pacman aura &>/dev/null && exit
 
 # Print both standard & AUR updates available
 printf ' %s | %s' "$(getSTD)" "$(getAUR)"
