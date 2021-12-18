584110d75a0ede6ea9ccd82c3d83e9f107424b25
Alex Paarfus <stewie410@gmail.com>
2021-12-18 12:32:03 -0500

    Small cleanup of template shell script

--- .vim/templates/skeleton.sh
+++ .vim/templates/skeleton.sh
@@ -4,43 +4,40 @@
 
 # Print message to terminal & logfile :: String type, String message
 printMsg() {
-    [ -n "${2}" ] || return
-    printf '[%s] [%s] %s\n' "$(date --iso-8601=sec)" "${1^^}" "${2}" | \
-        tee --append "${log}"
+	[ -n "${2}" ] || return
+	printf '[%s] [%s] %s\n' "$(date --iso-8601=sec)" "${1^^}" "${2}" | \
+		tee --append "${log}"
 }
-
 
 # Show Help/Usage
 show_help() {
-    cat << EOF
+	cat << EOF
 Description
 
 USAGE: ${0##*/} [OPTIONS]
 
 OPTIONS:
-    -h, --help      Show this help message
+	-h, --help		Show this help message
 EOF
 }
 
-# Variables
-log="/var/log/$(basename "${0##*/}")/$(date --iso-8601).log"
-trap 'unset log OPTS' EXIT
+main() {
+	local log opts
+	log="/var/log/$(basename "${0##*/}")/$(date --iso-8601).log"
 
-# Handle Arguments
-OPTS="$(getopt --options h --longoptions help --name "${0##*/}" -- "${@}")"
-eval set -- "${OPTS}"
-while true; do
-	case "${1}" in
-		-h | --help ) 		show_help; exit 0;;
-		-- ) 			    shift; break;;
-		* ) 			    break;;
-	esac
-    shift
-done
+	opts="$(getopt --options h --longoptions help --name "${0##*/}" -- "${@}")"
+	eval set -- "${opts}"
+	while true; do
+		case "${1}" in
+			-h | --help )		show_help; return 0;;
+			-- )				shift; break;;
+			* )					break;;
+		esac
+		shift
+	done
 
-# Generate Logfile
-mkdir --parents "${log%/*}"
-[ -s "${log}" ] && printf '\n\n%80s\n'  " " | tr ' ' "-" >> "${log}"
-touch -a "${log}"
+	mkdir --parents "${log%/*}"
+	touch -a "${log}"
+}
 
-# Run
+main "${@}"
