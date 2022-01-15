4cae47eb31f7d585529f399a26811f9e1135009c
Alex Paarfus <stewie410@gmail.com>
2022-01-15 14:13:51 -0500

    Use ssh instead of http(s)

--- scripts/tools/gitclone.sh
+++ scripts/tools/gitclone.sh
@@ -4,54 +4,61 @@
 
 # Show Help/Usage
 show_help() {
-    cat << EOF
+	cat << EOF
 Wrapper to simplify cloning from common git repository providers
 
-USAGE: ${0##*/} [OPTIONS] SLUG [DIRECTORY]
+USAGE: ${0##*/} [OPTIONS] SLUG
 
 OPTIONS:
-    -h, --help      Show this help message
-    -g, --github    Assume SLUG is hosted by github
-    -l, --gitlab    Assume SLUG is hosted by gitlab
-    -s, --suckless  Assume SLUG is hosted by suckless
+	-h, --help		Show this help message
+	-g, --github		Assume SLUG is hosted by github
+	-l, --gitlab		Assume SLUG is hosted by gitlab
+	-s, --suckless		Assume SLUG is hosted by suckless
+	-b, --base-url URL	Use URL as base url for SLUG
+	-d, --directory PATH	Output contents to PATH instead of './SLUG'
 
 SLUG:
-    Slug should follow the format 'owner/repo', for example:
+	Slug should follow the format 'owner/repo', for example:
 
-        stewie410/dotfiles
+		stewie410/dotfiles
 EOF
 }
 
-# Variables
-declare url
-trap 'unset url' EXIT
+main() {
+	local opts url dir
+	opts="$(getopt --options hglsd:b: --longoptions help,github,gitlab,suckless,directory:,base-url: --name "${0##*/}" -- "${@}")"
 
-# Handle Arguments
-OPTS="$(getopt --options hgls --longoptions help,github,gitlab,suckless --name "${0##*/}" -- "${@}")"
-eval set -- "${OPTS}"
-while true; do
-	case "${1}" in
-		-h | --help ) 		show_help; exit 0;;
-        -g | --github )     url="https://github.com";;
-        -l | --gitlab )     url="https://gitlab.com";;
-        -s | --suckless )   url="git://git.suckless.org";;
-		-- ) 			    shift; break;;
-		* ) 			    break;;
-	esac
-    shift
-done
+	eval set -- "${opts}"
+	while true; do
+		case "${1}" in
+			-h | --help )		show_help; return 0;;
+			-g | --github )		url="https://github.com";;
+			-l | --gitlab )		url="https://gitlab.com";;
+			-s | --suckless )	url="git://git.suckless.org";;
+			-b | --base-url )	url="${2}"; shift;;
+			-d | --directory )	dir="${2}"; shift;;
+			-- )				shift; break;;
+			* )					break;;
+		esac
+		shift
+	done
 
-# Require SLUG
-if [ -z "${1}" ]; then
-    printf '%s\n' "ERROR: No SLUG specified" >&2
-    exit 1
-fi
+	if [ -z "${*}" ]; then
+		printf '%s\n' "No slug specified" >&2
+		return 1
+	fi
 
-# Require SLUG host
-if [ -z "${url}" ]; then
-    printf '%s\n' "ERROR: No host specified" >&2
-    exit 1
-fi
+	if ! [[ "${*}" = */* ]]; then
+		printf '%s\n' "Invalid slug format: '${*}'" >&2
+		return 1
+	fi
 
-# Clone
-git clone "${url}/${1}" "${2:-${1##*/}}"
+	if [ -z "${url}" ]; then
+		printf '%s\n' "No base url specified for slugi: '${*}'" >&2
+		return 1
+	fi
+
+	git clone "${url}/${*}" "${dir:-${*##*/}}"
+}
+
+main "${@}"
