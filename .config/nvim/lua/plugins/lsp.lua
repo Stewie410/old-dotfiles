c0fe83cd3079b0a96d9897bfcc138507eae8a1af
Alex Paarfus <stewie410@gmail.com>
2023-07-09 00:48:37 -0400

    Fixed "gd" mapping to point to lsp-definition; added "gD" mapping for lsp-declaration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -52,7 +52,8 @@ return {
 
             lsp.on_attach(function(_, bufnr)
                 local opts = { buffer = bufnr, remap = false }
-                vim.keymap.set("n", "gd", function() vim.lsp.buf.destination() end, opts)
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
                 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                 vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
                 vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
