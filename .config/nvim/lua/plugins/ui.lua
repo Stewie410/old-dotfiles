ab90a1a65de171437ce13f79f77bf5c31b9d5265
Alex Paarfus <stewie410@gmail.com>
2023-07-14 16:55:46 -0400

    Removed active indent guides (mini.indentscope)

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -121,34 +121,8 @@ return {
             show_trailing_blankline_indent = false,
         },
     },
-    {
-        "echasnovski/mini.indentscope",
-        version = false,
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            symbol = "│",
-            options = { try_as_border = true },
-        },
-        init = function()
-            vim.api.nvim_create_autocmd({ "FileType" }, {
-                pattern = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-                callback = function()
-                    vim.b.miniindentscope_disable = true
-                end,
-            })
-        end,
-    },
+
+    -- terminal
 
     -- keymap display
     -- TODO register keybinds...
