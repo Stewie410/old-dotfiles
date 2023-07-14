72f8d3954ef3e5e38db5d260ee4fbf699117a6f3
Alex Paarfus <stewie410@gmail.com>
2023-07-14 16:57:38 -0400

    Added keybinds to gitsigns

--- .config/nvim/lua/plugins/vcs.lua
+++ .config/nvim/lua/plugins/vcs.lua
@@ -1,5 +1,41 @@
 return {
     -- git
-    { "tpope/vim-fugitive",      cmd = { "Git", "G" } },
-    { "lewis6991/gitsigns.nvim", opts = {} },
+    {
+        "tpope/vim-fugitive",
+        cmd = { "Git", "G" },
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            on_attached = function(buffer)
+                local gs = package.loaded.gitsigns
+
+                local function map(mode, lhs, rhs, desc)
+                    vim.keymap.set(mode, lhs, rhs, {
+                        buffer = buffer,
+                        desc = desc,
+                    })
+                end
+
+                -- stylua: ignore start
+                map("n", "]h", gs.next_hunk, "Next Hunk")
+                map("n", "[h", gs.prev_hunk, "Previous Hunk")
+                map({ "n", "v" }, "<leader>ghs", ":GitSigns stage_hunk<CR>", "Stage Hunk")
+                map({ "n", "v" }, "<leader>ghr", ":GitSigns reset_hunk<CR>", "Reset Hunk")
+                map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+                map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+                map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+                map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
+                map("n", "<leader>ghb", function()
+                    gs.blame_line({ full = true })
+                end, "Blame Line")
+                map("n", "<leader>ghd", gs.diffthis, "Diff This")
+                map("n", "<leader>ghD", function()
+                    gs.diffthis("~")
+                end, "Diff This ~")
+                map({ "o", "x" }, "ih", ":<C-U>GitSigns select_hunk<CR>", "GitSigns Select Hunk")
+            end,
+        },
+    },
 }
