6b4b13f44ca0e880e4b78d08bf78f7e06c35a6d4
Alex Paarfus <stewie410@gmail.com>
2023-07-14 14:21:37 -0400

    Use tables to define au/ag to simplify your life

--- .config/nvim/lua/core/functions.lua
+++ .config/nvim/lua/core/functions.lua
@@ -1,79 +1,75 @@
-local cmd = vim.api.nvim_create_user_command
 local au = vim.api.nvim_create_autocmd
 local ag = vim.api.nvim_create_augroup
 
 -- alias 'filetype detect'
-cmd("FTD", "filetype detect", {})
+vim.api.nvim_create_user_command("FTD", "filetype detect", {})
 
 -- trim trailing whitespace on write
 local group = ag("FormatOnSave", { clear = true })
-au("BufWritePre", {
-    pattern = { "*" },
-    group = group,
-    callback = function()
-        local pos = vim.fn.getpos(".")
-        vim.cmd([[%s/\s\+$//e]])
-        vim.fn.setpos(".", pos)
-    end,
-})
+local fmt_actions = {
+    {
+        callback = function()
+            local cursor = vim.fn.getpos(".")
+            vim.cmd([[%s/\s\+$//e]])
+            vim.fn.setpos(".", cursor)
+        end,
+        desc = "Trim trailing whitespace",
+    },
+    {
+        callback = function()
+            vim.lsp.buf.format()
+        end,
+        desc = "Apply LSP Formatting",
+    }
+}
 
--- LSP Format on save, if available
-au({ "BufWritePre" }, {
-    pattern = { "*" },
-    group = group,
-    callback = function() vim.lsp.buf.format() end,
-})
+for _, action in pairs(fmt_actions) do
+    au({ "BufWritePre" }, {
+        pattern = { "*" },
+        group = group,
+        callback = action.callback,
+        desc = action.desc,
+    })
+end
 
 -- Force-set filetype
-local group = ag("FileTypeAdjustments", { clear = true })
-au({ "BufRead", "BufNewFile" }, {
-    pattern = { "/tmp/calcurse*", "~/.calcurse/notes/*" },
-    group = group,
-    command = "set filetype=markdown",
-})
-au({ "BufRead", "BufNewFile" }, {
-    pattern = { "*.ms", "*.me", "*.mom", "*.man" },
-    group = group,
-    command = "set filetype=groff",
-})
-au({ "BufRead", "BufNewFile" }, {
-    pattern = { "*.tex" },
-    group = group,
-    command = "set filetype=tex",
-})
-au({ "BufRead", "BufNewFile" }, {
-    pattern = { "*.sh", "*.bash" },
-    group = group,
-    command = "set filetype=sh",
-})
+group = ag("FileTypeAdjustments", { clear = true })
+local filetypes = {
+    ["groff"] = { "*.ms", "*.me", "*.mom", "*.man" },
+    ["tex"] = { "*.tex" },
+    ["sh"] = { "*.sh", "*.bash" },
+}
+
+for ft, pattern in pairs(filetypes) do
+    au({ "BufRead", "BufNewFile" }, {
+        pattern = pattern,
+        group = group,
+        command = "set filetype=" .. ft,
+        desc = "Set filetype to '" .. ft .. "'",
+    })
+end
 
 -- Support comments in json
 au({ "FileType" }, {
     pattern = { "json" },
     group = ag("CustomCommentSupport", { clear = true }),
     command = [[syntax match Comment +\/\/.\+$++]],
+    desc = "Add comment support to JSON",
 })
 
 -- Native templates
-local group = ag("TemplateFiles", { clear = true })
-local template = "~/.config/templates/template"
-au({ "BufNewFile" }, {
-    pattern = { "*.sh", "*.bash" },
-    group = group,
-    command = "0r " .. template .. ".sh",
-})
-au({ "BufNewFile" }, {
-    pattern = { "*.bat", "*.cmd" },
-    group = group,
-    command = "0r " .. template .. ".bat",
-})
-au({ "BufNewFile" }, {
-    pattern = { "*.ahk" },
-    group = group,
-    command = "0r " .. template .. ".ahk",
-})
-au({ "BufNewFile" }, {
-    pattern = { "*.ps1" },
-    group = group,
-    command = "0r " .. template .. ".ps1",
-})
+group = ag("TemplateFiles", { clear = true })
+local templates = {
+    ["sh"] = { "*.sh", "*.bash" },
+    ["bat"] = { "*.bat", "*.cmd" },
+    ["ahk"] = { "*.ahk" },
+    ["ps1"] = { "*.ps1", "*.psm1" },
+}
+for ext, pattern in pairs(templates) do
+    au({ "BufNewFile" }, {
+        pattern = pattern,
+        group = group,
+        command = "0r ~/.config/templates/template." .. ext,
+        desc = "Load template for '" .. ext .. "'",
+    })
+end
