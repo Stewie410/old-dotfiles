47c7a7aaf2f0ba40f8726024cbe97f9982edc26c
Alex Paarfus <stewie410@gmail.com>
2023-07-14 16:34:35 -0400

    Added mini.indentscope

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -121,6 +121,34 @@ return {
             show_trailing_blankline_indent = false,
         },
     },
+    {
+        "echasnovski/mini.indentscope",
+        version = false,
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            symbol = "│",
+            options = { try_as_border = true },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd({ "FileType" }, {
+                pattern = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+                callback = function()
+                    vim.b.miniindentscope_disable = true
+                end,
+            })
+        end,
+    },
 
     -- keymap display
     -- TODO register keybinds...
