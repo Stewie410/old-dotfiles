078a0d5136ba9eeccbab9d822edb1773030a4ac4
Alex Paarfus <stewie410@gmail.com>
2023-07-17 12:18:38 -0400

    Switch to 'numToStr/Comment.nvim' for commenting needs, as it'll cover most cases anyway

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,33 +1,10 @@
 return {
     -- list ux
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
+    { "folke/trouble.nvim" },
 
     -- comments
-    {
-        "folke/todo-comments.nvim",
-        lazy = false,
-        opts = {},
-    },
-    {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        lazy = true,
-    },
-    {
-        "echasnovski/mini.comment",
-        version = "*",
-        event = "VeryLazy",
-        opts = {
-            options = {
-                custom_commentstring = function()
-                    local cs = require("ts_context_commentstring.internal")
-                    return cs.calculate_commentstring() or vim.bo.commentstring
-                end,
-            },
-        },
-    },
+    { "folke/todo-comments.nvim", opts = {} },
+    { "numToStr/Comment.nvim",    opts = {} },
 
     -- undo
     {
