8a75d50d2bcb604ccf560b7a59b28f3eed90a1f5
Alex Paarfus <stewie410@gmail.com>
2023-07-17 11:58:18 -0400

    Switch to "numToStr/Navigator.nvim" for nvim/tmux integration

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -39,5 +39,40 @@ return {
     },
 
     -- navigation
-    { "christoomey/vim-tmux-navigator", lazy = false },
+    {
+        "numToStr/Navigator.nvim",
+        keys = {
+            {
+                "<C-h>",
+                function() require("Navigator").left() end,
+                desc = "Navigate left",
+                mode = { "n", "t" },
+            },
+            {
+                "<C-j>",
+                function() require("Navigator").down() end,
+                desc = "Navigate down",
+                mode = { "n", "t" },
+            },
+            {
+                "<C-k>",
+                function() require("Navigator").up() end,
+                desc = "Navigate up",
+                mode = { "n", "t" },
+            },
+            {
+                "<C-l>",
+                function() require("Navigator").right() end,
+                desc = "Navigate right",
+                mode = { "n", "t" },
+            },
+            {
+                "<C-\\>",
+                function() require("Navigator").previous() end,
+                desc = "Navigate to previous split/pane",
+                mode = { "n", "t" },
+            },
+        },
+        opts = {},
+    }
 }
