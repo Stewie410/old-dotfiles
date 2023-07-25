88d045b1aa16c3b8713080de449237d2de41a222
Alex Paarfus <stewie410@gmail.com>
2023-07-25 14:25:14 -0400

    Moved code back to 'global' scope, to ensure function

--- .config/bash/wsl.sh
+++ .config/bash/wsl.sh
@@ -3,11 +3,6 @@
 # Start wsl-vpnkit for DNS & Networking support with VPNs...
 # https://github.com/sakai135/wsl-vpnkit
 
-vpnkit() {
-    command -v "wsl.exe" &>/dev/null || return
-    wsl.exe --list | grep --quiet 'wsl-vpnkit' || return
+if wsl.exe --list | grep --quiet 'wsl-vpnkit'; then
     wsl.exe --distribution "wsl-vpnkit" "service wsl-vpnkit start"
-}
-
-vpnkit
-unset -f vpnkit
+fi
