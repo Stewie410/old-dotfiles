d1eb7dbca72ba87d88b878b42df57a011cae8751
Alex Paarfus <stewie410@gmail.com>
2023-09-22 13:42:25 -0400

    Revert nnvim+tmux integration

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -14,42 +14,4 @@ return {
             vim.g.undotree_SetFocusWhenToggle = true
         end,
     },
-
-    -- navigation
-    {
-        "numToStr/Navigator.nvim",
-        keys = {
-            {
-                "<C-h>",
-                function() require("Navigator").left() end,
-                desc = "Navigate left",
-                mode = { "n", "t" },
-            },
-            {
-                "<C-j>",
-                function() require("Navigator").down() end,
-                desc = "Navigate down",
-                mode = { "n", "t" },
-            },
-            {
-                "<C-k>",
-                function() require("Navigator").up() end,
-                desc = "Navigate up",
-                mode = { "n", "t" },
-            },
-            {
-                "<C-l>",
-                function() require("Navigator").right() end,
-                desc = "Navigate right",
-                mode = { "n", "t" },
-            },
-            {
-                "<C-\\>",
-                function() require("Navigator").previous() end,
-                desc = "Navigate to previous split/pane",
-                mode = { "n", "t" },
-            },
-        },
-        opts = {},
-    }
 }
